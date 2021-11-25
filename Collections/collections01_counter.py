c1914d0a36b71d4d6927394adac4b4536cba01de
YoonEunkyeong <rainbow6468@gmail.com>
2021-11-25 20:52:59 +0900

    practice_211125

--- Collections/collections01_counter.py
+++ Collections/collections01_counter.py
@@ -19,7 +19,7 @@ container.update("cccccccccccccc")
 print(container)
 
 for n in "abccdfe":
-    print('%s:%d' %(n, container[n]), end=" ")
+    print('%s:%d' % (n, container[n]), end=" ")
 print()
 
 ct = collections.Counter("Hello")
@@ -55,3 +55,74 @@ ct4 = collections.Counter('aeroplane')
 print(ct4.values())
 print((ct3 - ct4))
 print((ct3 & ct4))
+
+list_test = [i for i in range(10)]
+list_test.pop(8)
+print(list_test)
+
+print(8 in list_test)
+
+print(sorted('Python'))
+
+tuple_test = (2, 3, 4)
+print(tuple_test)
+
+test = [1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 5]
+set_test = set(test)
+print(set_test)
+set_test.add(19)
+set_test.remove(3)
+print(set_test)
+
+sample_dict = {1: 'katie', 2: 'changho', 3: 'zichang'}
+print(sample_dict.get(1, 0))
+print(sample_dict.get(4, 0))
+print(sample_dict.keys())
+
+check_list = [3, 4, 5, 6]
+
+for i in sample_dict:
+    print(i in check_list)
+
+del (sample_dict[1])
+print(sample_dict)
+
+print(3 in sample_dict)
+
+result = 0 or 100  # 참인데... or 의 결과가 True 가 아니다?  int 100 이다?
+print(result)
+result = "Hello" or "Python"  # "Hello"
+print(result)
+result = "Hello" and "Python"  # "Python"
+print(result)
+result = [] and "Python"  # []
+print(result)
+result = {'name': 'yoon'} or (1, 2, 3)
+print(result)
+
+a, b = divmod(13, 3)
+print(a)
+
+print(list_test)
+for i, j in enumerate(list_test):
+    print(i, j)
+
+print(pow(2, 5))
+
+from functools import reduce
+
+
+def add(x, y):
+    print((x, y))
+    return x + y
+
+
+print(reduce(lambda x, y: x + y, [1, 3, 5, 7, 9]))
+
+
+def func(x, y):
+    x.append(y ** 2)
+    return x
+
+
+print(reduce(func, [1, 4, 9], []))
